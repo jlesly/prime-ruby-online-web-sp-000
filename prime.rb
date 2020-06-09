c9c5e6d492465fcc176e5dfd59131586e6eff5d3
Lesly Juarez <leslyj2@gmail.com>
2020-06-09 21:04:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,13 @@
-def prime?(x)
-  if x == 1
-    return true 
-  elsif x < 1
-    return false
-  else
-  for y in 2..(x-1)
-    if (x % y) == 0
-      return false 
+def get_prime_no_upto(number)
+  start = 2
+  primes = (start..number).to_a
+  (start..number).each do |no|
+    (start..no).each do |num|
+      if ( no % num  == 0) && num != no
+        primes.delete(no)
+        break
+      end
     end
-  end 
+  end
+  primes
 end
-
-  true
-end
-
-it 'returns false for non-prime numbers' do
-    expect(prime?(0)).to be(false)	    #expect(prime?(0)).to be(false)
-    expect(prime?(1)).to be(false)	    #expect(prime?(1)).to be(false)
-    expect(prime?(4)).to be(false)	    expect(prime?(4)).to be(false)
-    expect(prime?(40)).to be(false)	    expect(prime?(40)).to be(false)
-    expect(prime?(101013)).to be(false)	    expect(prime?(101013)).to be(false)
