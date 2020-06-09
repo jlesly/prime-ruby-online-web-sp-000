2ce8b16c61e80e2fb806d3f6d23904200c1c60eb
Lesly Juarez <leslyj2@gmail.com>
2020-06-09 21:08:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,2 @@
-def prime?(number)
- check = false
- count = 2
-def prime?(number)
- return check
-end	
-end
+prime_array = Prime.take_while
 
-def prime?(number)
-  range = (2..(number - 1)).to_a
-  check = true
-  if number < 2
-    check = false
-  else
-    range.each do |x|
-      if number % x == 0
-        check = false
-        break
-      end
-    end
-  end
-   puts check
-end
